9c69b64c439a02019a1d8882f0d9eec746588299
maalieva-webdev <guli.maalievat@gmail.com>
2025-05-08 12:42:05 +0600

    first commit

---
+++ src/component/Header/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "./img/logotip.svg";
+import phone from "./img/zvonok.svg";
+import car from "./img/karzina.svg";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <img className="logo" src={logo} alt="" />
+        <div className="nav-bar">
+          <NavLink to={"/"}>Главная</NavLink>
+          <NavLink to={"/shop"}>Магазин</NavLink>
+          <NavLink to={"/brend"}>О бренде</NavLink>
+          <NavLink to={"/contact"}>Контакты</NavLink>
+        </div>
+        <div>
+          <Link className="tel">
+            {" "}
+            <img src={phone} alt="" />
+            +7 (495) 823-54-12
+          </Link>
+        </div>
+        <img className="car" src={car} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
